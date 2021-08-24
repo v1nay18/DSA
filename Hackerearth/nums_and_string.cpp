de7d396a2b04bfdec3475fca1ed5b2becd3468dd
Vinay Kumar Sinha <123456vvv789@gmail.com>
2021-08-24 10:11:50 +0530

    Regular Commit

---
+++ Hackerearth/nums_and_string.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int two_times(int n)
+{
+    return 2 * n;
+}
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n;
+    cin >> s;
+    cout << two_times(n) << endl;
+    cout << s << endl;
+
+    return 0;
+}
